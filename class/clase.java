d3d05d269fb06a33a594e6f5abd839148775841c
argoxt <sboteros205@gmail.com>
2025-09-03 17:40:52 -0500

    Add files via upload

---
+++ class/clase.java
@@ -0,0 +1,31 @@
+import javax.print.DocFlavor.STRING;
+
+public class clase {
+
+
+
+    
+    public static void main(String[] args) {
+
+        int numerob = 50;
+        byte numerito = 100;
+        short numeroshor = 2345;
+        long numerolongo = 234235289; 
+        double numerodouble = 2;
+        String nombres = "santiago";
+        System.out.println("hola mundo");
+        System.out.println(numerob);
+        System.out.println(numeroshor);
+        System.out.println(numerolongo);
+        System.out.println(numerito);
+        System.out.println(numerodouble);
+System.out.println("hola "+ nombres);
+
+
+
+
+
+
+
+    }
+}
